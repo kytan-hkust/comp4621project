c1504ead73c466ed855bfd2f9b0ecaa54e5103b8
Tan Ke Yi <kytan@connect.ust.hk>
2020-05-17 03:02:39 +0800

    More correct implementations...

--- proxy.c
+++ proxy.c
@@ -8,8 +8,9 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define PORT    12345
-#define LISTENQ 1024
+#define LISTENQ  1024
+#define MAX_LINE 8192
+#define PORT     12345
 
 
 int starts_with(const char* s, const char* prefix) {
@@ -73,14 +74,77 @@ int parse_absolute_url(const char* absolute_url, char* hostname, char* pathname)
 
 /* Naive Access Control */
 int is_blocked(const char* hostname) {
-    const char* blocked_list[3] = {"sample.com", "example.com", "google.com"};
+    const char* blocked_list[2] = {"sing.cse.ust.hk", "google.com"};
 
     int i;
-    for (i = 0; i < 3; ++i) {
+    for (i = 0; i < 2; ++i) {
         if (strcmp(hostname, blocked_list[i]) == 0) return 1;
     }
 
     return 0;
+}
+
+
+void reply_not_found(int in_fd, const char* version) {
+    char buffer[MAX_LINE];
+    snprintf(buffer, sizeof(buffer), "%s 404 Not Found\r\n\r\n", version);
+    write(in_fd, buffer, strlen(buffer));
+}
+
+
+void reply_not_implemented(int in_fd, const char* version) {
+    char buffer[MAX_LINE];
+    snprintf(buffer, sizeof(buffer), "%s 501 Not Implemented\r\n\r\n", version);
+    write(in_fd, buffer, strlen(buffer));
+}
+
+
+int read_request(int in_fd, char* request) {
+    int i = 0, j, n;
+    char c, c4[5];
+
+    while (i < MAX_LINE) {
+        n = read(in_fd, &c, 1);
+        if (n < 0) {
+            printf("[ERROR]\tSocket read failed\n");
+            return 1;
+        }
+        else if (n == 0) break;
+        request[i++] = c;
+
+        for (j = 0; j < 4; ++j) c4[j] = request[i - 4 + j];
+        if (strcmp(c4, "\r\n\r\n") == 0) break;
+    }
+    request[i] = '\0';
+    return 0;
+}
+
+
+void handle_request(int in_fd) {
+    char request[MAX_LINE];
+    read_request(in_fd, request);
+
+    char method[8], target[2048], version[16];
+    sscanf(request, "%s %s %s\r\n", method, target, version);
+
+    char hostname[2048], pathname[2048];
+    parse_absolute_url(target, hostname, pathname);
+
+    if (strcmp(method, "GET") == 0) {
+        if (is_blocked(hostname)) reply_not_found(in_fd, version);
+        else {
+            //
+        }
+    }
+    else if (strcmp(method, "CONNECT") == 0) {
+        if (is_blocked(hostname)) reply_not_found(in_fd, version);
+        else {
+            //
+        }
+    }
+    else reply_not_implemented(in_fd, version);
+
+    close(in_fd);
 }
 
 
@@ -109,7 +173,12 @@ int main() {
     }
 
     while (1) {
-        break;
+        if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) < 0) {
+            printf("[ERROR]\tSocket accept failed\n");
+            return 0;
+        }
+
+        handle_request(connfd);
     }
 
     return 0;
